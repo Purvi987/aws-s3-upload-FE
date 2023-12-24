6d4255e0b87fb287b3d58b50503340b3e7791256
Purvi987 <purvibarot987@gmail.com>
2023-12-23 23:20:26 -0800

    Initial commit

---
+++ frontend/.angular/cache/17.0.7/vite/deps/@angular_platform-browser_animations.js
@@ -0,0 +1,23 @@
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-OY5NSGOZ.js";
+import "./chunk-GFNQAS2T.js";
+import "./chunk-FUYMHUEZ.js";
+import "./chunk-BCUU2HZG.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-SLQHMPVU.js";
+import "./chunk-ENBES6XL.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
